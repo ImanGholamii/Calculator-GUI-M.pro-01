4f5e412ecc65c5d83bcdaef86c2fffe26bc6584c
Iman <imangholamiimi@gmail.com>
2024-05-23 17:36:32 +0330

    :zap: | UPDATE | add 'resize' and 'evaluate_results' methods

--- calculator-01/main.py
+++ calculator-01/main.py
@@ -25,4 +25,15 @@ class MyApp(App):
             
         for button in button_grid.children[1:]: #  iterate over all buttons in the button_grid except the first one (which is the clear button). 
             button.bind(on_press=print_button_text) # We bind the print_button_text function to the on_press event of each button. #  iterate over all buttons in the button_grid except the first one (which is the clear button). 
-            
+        
+        def resize_lable_text(lable, new_height):
+            lable.fontsize = 0.5*lable.height
+        output_lable.bind(height=resize_lable_text)
+        
+        def evaluate_resulte(instance):
+            try:
+                output_lable.text = str(eval(output_lable.text))
+            except SyntaxError:
+                output_lable.text = 'Python Syntax Error!'
+        button_grid.children[0].bind(on_press=evaluate_result)
+                
